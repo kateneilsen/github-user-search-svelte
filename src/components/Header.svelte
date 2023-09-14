ec22a58c772265dc57ce78c1a88fcb120f727fa1
Kate <neilsenk09@gmail.com>
2023-09-14 10:36:25 -0400

    custom store for theme
    reference: https://maximmaeder.com/custom-store-for-svelte/

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,6 +3,15 @@
   const dispatch = createEventDispatcher();
 
   export let isDarkMode = true;
+
+  import { colorScheme } from "../stores";
+
+  // Set the color scheme each time it changes.
+  // Doing it this way could be useful if you use Tailwind with the class strategy.
+  $: {
+    document.firstElementChild.setAttribute("class", $colorScheme);
+    document.firstElementChild.style.colorScheme = $colorScheme;
+  }
 </script>
 
 <div class="header">
@@ -10,12 +19,14 @@
   <h6>
     <button
       class="color-scheme {isDarkMode ? 'dark' : ''}"
-      on:click={() => (isDarkMode = !isDarkMode)}
+      on:click={colorScheme.toggle}
     >
-      {isDarkMode ? "Light" : "Dark"}
+      {$colorScheme === "light" ? "Light" : "Dark"}
       <img
-        src={isDarkMode ? "assets/icon-sun.svg" : "assets/icon-moon.svg"}
-        alt={isDarkMode ? "sun" : "moon"}
+        src={$colorScheme === "light"
+          ? "assets/icon-sun.svg"
+          : "assets/icon-moon.svg"}
+        alt={$colorScheme === "light" ? "sun" : "moon"}
       />
     </button>
   </h6>
---
+++ src/stores.js
@@ -0,0 +1,34 @@
+import { writable } from "svelte/store";
+
+// Get the OS default color scheme
+const media_query = matchMedia("(prefers-color-scheme: dark)");
+const os_default = media_query.matches ? "dark" : "light";
+
+// Get the current color scheme from local storage, or default to light
+const { subscribe, update } = writable(
+  localStorage.getItem("colorScheme") ?? os_default
+);
+
+// Create a channel so we can communicate with other tabs/windows/iframes
+const channel = new BroadcastChannel("colorScheme");
+
+// Listen for messages from other tabs/windows/iframes
+channel.addEventListener("message", (event) => {
+  update(() => event.data);
+});
+
+const toggle = () =>
+  update((n) => {
+    const newColorScheme = n === "light" ? "dark" : "light";
+
+    // Save the new color scheme to local storage
+    localStorage.setItem("colorScheme", newColorScheme);
+
+    // Send the new color scheme to other tabs/windows/iframes
+    channel.postMessage(newColorScheme);
+
+    return newColorScheme;
+  });
+
+// A store object must return a subscribe function, all other properties are optional.
+export const colorScheme = { subscribe, toggle };
